69b26b1a0be65576351649fe420fb920a741aa69
ec-cvdl <kollivier@emmaus-connect.org>
2025-10-07 10:59:52 +0200

    Update mintpostinstall.sh

--- mintpostinstall.sh
+++ mintpostinstall.sh
@@ -1,41 +1,41 @@
 # Mise à jour du système
-echo "Mise à jour du système, et récupération des derniers paquets depuis les serveurs officiels ..."
+echo -e "\e[32;43mVert sur fond jaune\e[0m" "Mise à jour du système, et récupération des derniers paquets depuis les serveurs officiels ..."
 sudo apt update && sudo apt upgrade -y
 sudo apt autoremove -y
 
 # Interface et raccourcis
-echo "Affichage du dossier personnel sur le Bureau ..."
+echo -e "\e[32;43mVert sur fond jaune\e[0m" "Affichage du dossier personnel sur le Bureau ..."
 gsettings set org.nemo.desktop home-icon-visible true
 
-echo "Affichage de la corbeille sur le Bureau"
+echo -e "\e[32;43mVert sur fond jaune\e[0m" "Affichage de la corbeille sur le Bureau"
 gsettings set org.nemo.desktop trash-icon-visible true
 nemo -q
 nemo &
 
 # Installation de paquets essentiels
-echo "Désinstallation du lecteur média Celluloid"
+echo -e "\e[32;43mVert sur fond jaune\e[0m" "Désinstallation du lecteur média Celluloid"
 sudo apt remove celluloid -y
 sudo apt purge celluloid -y
 
 # Installation de VLC Media Player
-echo "Installation de VLC Media Player à la place de Celluloid ..."
+echo -e "\e[32;43mVert sur fond jaune\e[0m" "Installation de VLC Media Player à la place de Celluloid ..."
 sudo apt install vlc -y
 
 # Ajout et vérification de la présence du support Flatpak
-echo "Vérification de la présence de Flatpak ..."
+echo -e "\e[32;43mVert sur fond jaune\e[0m" "Vérification de la présence de Flatpak ..."
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Vérification de la présence de Libre Office (si non, installation)
-echo "Vérification de la présence de Libre Office ..."
+echo -e "\e[32;43mVert sur fond jaune\e[0m" "Vérification de la présence de Libre Office ..."
 if ! command -v libreoffice &> /dev/null; then
-    echo "LibreOffice n'est pas installé. Installation en cours..."
+    echo -e "\e[32;43mVert sur fond jaune\e[0m" "LibreOffice n'est pas installé. Installation en cours..."
     sudo apt update
     sudo apt install -y libreoffice
 else
-    echo "LibreOffice est déjà installé."
+    echo -e "\e[32;43mVert sur fond jaune\e[0m" "LibreOffice est déjà installé."
 fi
 
 # Installation des polices Microsoft
-echo "Installation des polices Microsoft pour éviter les problèmes de compatibilité ..."
+echo -e "\e[32;43mVert sur fond jaune\e[0m" "Installation des polices Microsoft pour éviter les problèmes de compatibilité ..."
 sudo apt install ttf-mscorefonts-installer -y
 

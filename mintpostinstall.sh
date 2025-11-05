3425d00790c493428db4b818fbfa27e509ff2982
ec-cvdl <kollivier@emmaus-connect.org>
2025-11-05 18:47:18 +0100

    Supression temporaire des messages dans le terminal

--- mintpostinstall.sh
+++ mintpostinstall.sh
@@ -1,25 +1,20 @@
 # Mise à jour du système
-echo -e "\e[32;43mMise à jour du système, et récupération des derniers paquets depuis les serveurs officiels ...\e[0m"
 sudo apt update && sudo apt upgrade -y
 sudo apt autoremove -y
 
 
 
 # Installation de paquets essentiels
-echo "\e[36mDésinstallation du lecteur média Celluloid\e[0m"
 sudo apt remove celluloid -y
 sudo apt purge celluloid -y
 
 # Installation de VLC Media Player
-echo -e "\e[32;43mVert sur fond jaune\e[0m" "Installation de VLC Media Player à la place de Celluloid ..."
 sudo apt install vlc -y
 
 # Ajout et vérification de la présence du support Flatpak
-echo -e "\e[32;43mVert sur fond jaune\e[0m" "Vérification de la présence de Flatpak ..."
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Vérification de la présence de Libre Office (si non, installation)
-echo -e "\e[32;43mVert sur fond jaune\e[0m" "Vérification de la présence de Libre Office ..."
 if ! command -v libreoffice &> /dev/null; then
     echo -e "\e[32;43mVert sur fond jaune\e[0m" "LibreOffice n'est pas installé. Installation en cours..."
     sudo apt update
@@ -29,7 +24,6 @@ else
 fi
 
 # Ajout des raccourcis sur le Bureau
-echo "\e[36mAffichage du dossier personnel sur le Bureau, et des raccourcis logiciels ..."
 gsettings set org.nemo.desktop home-icon-visible true
 
 echo -e "\e[32;43mVert sur fond jaune\e[0m" "Affichage de la corbeille sur le Bureau"

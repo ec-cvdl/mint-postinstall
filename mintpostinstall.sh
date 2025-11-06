e9cc8a3f692830000ded70a4c6dab0029c831cb1
ec-cvdl <kollivier@emmaus-connect.org>
2025-11-06 05:22:08 +0100

    Reformulation du fichier

--- mintpostinstall.sh
+++ mintpostinstall.sh
@@ -1,1 +1,33 @@
+# Mise à jour du système
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove
+
+# Désinstallation de Celluloid
+sudo apt remove celluloid -y
+sudo apt purge celluloid -y
+
+#Installation de VLC Media Player
+sudo apt install vlc -y
+
+# Vérification du support de Flatpak
+flatpak remote-ad --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Ajout des raccourcis sur le Bureau
+gsettings set org.nemo.desktop home-icon-visible true
+gsettings set org.nemo.desktop trash-icon-visible true
+# Libre Office
+[Desktop Entry]
+Name=Libre Office
+Exec=libreoffice %U
+Icon=libreoffice-main
+Type=Application
+Terminal=false
+Categories=Office
+
+# Rendre les raccourcis exécutables
+chmod +x ~/Bureau/*.desktop
+
+# Redémarrage de l'explorateur
+nemo -q
 

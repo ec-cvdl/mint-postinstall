46bbe0a3cc222a344dfe771c856c463b776369b5
ec-cvdl <kollivier@emmaus-connect.org>
2025-11-06 07:35:42 +0100

    Ajout des raccourcis bureau (test)

--- mintpostinstall.sh
+++ mintpostinstall.sh
@@ -18,12 +18,59 @@ gsettings set org.nemo.desktop home-icon-visible true
 gsettings set org.nemo.desktop trash-icon-visible true
 # Libre Office
 [Desktop Entry]
-Name=Libre Office
-Exec=libreoffice %U
-Icon=libreoffice-main
+Version=1.0
 Type=Application
+Name=LibreOffice 
+Comment=Ouvrir LibreOffice 
+Exec=libreoffice 
+Icon=libreoffice
+Path=
 Terminal=false
-Categories=Office
+StartupNotify=false
+# VLC
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=VLC
+Comment=Lecteur multimédia VLC
+Exec=vlc
+Icon=vlc
+Path=
+Terminal=false
+StartupNotify=false
+# Logithèque
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Logithèque
+Comment=Gestionnaire de logiciels Linux Mint
+Exec=mintinstall
+Icon=software-manager
+Path=
+Terminal=false
+StartupNotify=false
+# Gestionnaire de mise à jour
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Gestionnaire de mise à jour
+Comment=Gestionnaire de mise à jour Linux Mint
+Exec=mintupdate
+Icon=software-update-available
+Path=
+Terminal=false
+StartupNotify=false
+# Firefox
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Firefox
+Comment=Navigateur web Firefox
+Exec=firefox
+Icon=firefox
+Path=
+Terminal=false
+StartupNotify=false
 
 # Rendre les raccourcis exécutables
 chmod +x ~/Bureau/*.desktop

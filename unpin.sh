ec51aaffc0827f9ff906b9729d9e86f1c02f5847
ec-cvdl <kollivier@emmaus-connect.org>
2025-11-18 11:09:30 +0100

    Update unpin.sh

--- unpin.sh
+++ unpin.sh
@@ -1,6 +1,6 @@
 gsettings set org.cinnamon favorite-apps "$( \
     gsettings get org.cinnamon favorite-apps | \
     tr -d "[]'" | tr ',' '\n' | \
-    grep -v '^code\.desktop$' | \
+    grep -v '^terminal\.desktop$' | \
     awk '{printf "\"%s\",",$0}' | sed 's/,$//'; \
     echo )"

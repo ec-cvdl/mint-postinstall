243002f02866969c841fb6213f34858845fee53e
ec-cvdl <kollivier@emmaus-connect.org>
2026-01-29 11:02:49 +0100

    Update mintpostinstall.sh

--- mintpostinstall.sh
+++ mintpostinstall.sh
@@ -151,7 +151,7 @@ gsettings set org.cinnamon.desktop.background picture-uri "file:///usr/share/bac
 # Application du thème Clair
 gsettings set org.cinnamon.desktop.interface gtk-theme "Mint-Y-Aqua"
 gsettings set org.cinnamon.desktop.interface icon-theme "Mint-Y-Aqua"
-gsettings set org.cinnamon.theme name "Mint-Y"
+gsettings set org.cinnamon.theme name "Mint-Y- Aqua"
 gsettings set org.cinnamon.desktop.wm.preferences theme "Mint-Y-Aqua"
 
 # Création du dossier .mozzila dans le dossier personnel
